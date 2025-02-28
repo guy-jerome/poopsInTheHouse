f9baa84caff5124a610dc501bc0c6b75417ec321
guy-jerome <aaronjamesroberts@gmail.com>
2025-02-27 22:04:42 -0500

    added the counter tmp

--- src/app/app.js
+++ src/app/app.js
@@ -1,8 +1,8 @@
 import angular from "angular"; // Import AngularJS
 import MainController from "./controllers/mainController"; // Import the controller
-
 // Define the AngularJS module
 const app = angular.module("myApp", []);
 
 // Register the controller
 app.controller("mainController", MainController);
+app.directive("test", test);
